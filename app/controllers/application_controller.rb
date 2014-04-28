c4f8ccf09d2a1b1104d0f3192db0101f0bbfe154
Aidan <yidong.guan@gmail.com>
2014-04-28 10:45:19 +0800

    在app controller中允许所有的访问

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  before_filter :add_cors_headers
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:sign_up) << :avatar
+  end
+
+  def add_cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET,OPTIONS'
   end
 end
